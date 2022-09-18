224c5628be3d306411e96b3252c4789b8314eb4c
JM Benedetto <jmbenedetto@gmail.com>
2022-09-18 19:58:59 -0300

    solve bucket from GitHub_repository

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,12 @@ echo "$INPUT_SECRETS" > /secrets.json
 gcloud auth activate-service-account --key-file=/secrets.json
 rm /secrets.json
 
+#identify bucket name
+BUCKET=$(echo $GITHUB_HEPOSITORY| cut -d'/' -f 2)
+
 # Sync files to bucket
 echo "Syncing bucket $BUCKET ..."
-gsutil -m rsync -r -c -d -x "$INPUT_EXCLUDE" /github/workspace/$BUCKET gs://$INPUT_BUCKET/TEST
+gsutil -m rsync -r -c -d -x "$INPUT_EXCLUDE" /github/workspace/$BUCKET gs://$INPUT_BUCKET/$BUCKET
 if [ $? -ne 0 ]; then
     echo "Syncing failed"
     exit 1
