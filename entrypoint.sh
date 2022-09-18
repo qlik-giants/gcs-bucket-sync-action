60b8781c86d057c5469ecc9534cff1e2a0a4e60d
JM Benedetto <jmbenedetto@gmail.com>
2022-09-18 20:04:14 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ BUCKET=$(echo $GITHUB_REPOSITORY| cut -d'/' -f 2)
 
 # Sync files to bucket
 echo "Syncing bucket $BUCKET ..."
-gsutil -m rsync -r -c -d -x "$INPUT_EXCLUDE" /github/workspace/$BUCKET gs://$INPUT_BUCKET/$BUCKET
+gsutil -m rsync -r -c -d -x "$INPUT_EXCLUDE" /github/workspace gs://$INPUT_BUCKET/$BUCKET
 if [ $? -ne 0 ]; then
     echo "Syncing failed"
     exit 1
