bb88ccf79eabddd852cf8835cb484c166687259f
JM Benedetto <jmbenedetto@gmail.com>
2022-09-19 19:22:22 -0300

    Revert "Include branch name within the target path"
    This reverts commit 4a2c0b97e9959823173e41f6faf98e5a7e9c646a.

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,11 +5,11 @@ gcloud auth activate-service-account --key-file=/secrets.json
 rm /secrets.json
 
 #identify bucket name
-REPO=$(echo $GITHUB_REPOSITORY| cut -d'/' -f 2)
+BUCKET=$(echo $GITHUB_REPOSITORY| cut -d'/' -f 2)
 
 # Sync files to bucket
-echo "Syncing bucket $REPO ..."
-gsutil -m rsync -r -c -d -x "$INPUT_EXCLUDE" /github/workspace gs://$INPUT_BUCKET/$REPO/$GITHUB_REF_NAME
+echo "Syncing bucket $BUCKET ..."
+gsutil -m rsync -r -c -d -x "$INPUT_EXCLUDE" /github/workspace gs://$INPUT_BUCKET/$BUCKET
 if [ $? -ne 0 ]; then
     echo "Syncing failed"
     exit 1
